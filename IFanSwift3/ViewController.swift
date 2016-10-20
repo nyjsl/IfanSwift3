b28c037696c56f1a67804ab7c7fe786e13700813
= <=>
2016-10-20 21:39:08 +0800

    Initial Commit

---
+++ IFanSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IFanSwift3
+//
+//  Created by 魏星 on 2016/10/20.
+//  Copyright © 2016年 wx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
